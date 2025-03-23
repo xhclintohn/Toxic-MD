d4cf519db04babb765e425c23441673e1dce30d5
𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 <152104512+xhclintohn@users.noreply.github.com>
2025-03-23 17:59:47 +0300

    Update toxic.js

--- toxic.js
+++ toxic.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 app.get('/', (req, res) => {
   res.send('Toxic-MD bot is running');
 });
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
