9fbac33f11405ca189e5fc25d0659810a54531da
𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 <152104512+xhclintohn@users.noreply.github.com>
2025-04-07 05:51:06 +0300

    Update toxic.js

--- commandes/toxic.js
+++ commandes/toxic.js
@@ -1,1 +1,16 @@
-</>
+zokou({
+    nomCom: "bot",  /
+    categorie: "General",
+    reaction: "🤖" 
+}, async (dest, zk, commandeOptions) => {
+    const { repondre } = commandeOptions;
+    
+   
+    const fancyResponse = `
+𝐇𝐞𝐥𝐥𝐨! 𝐇𝐨𝐰 𝐜𝐚𝐧 𝐈 𝐡𝐞𝐥𝐩🙂🤚🏻
+    
+> ✗𝗗
+    `;
+    
+    await repondre(fancyResponse);
+});
