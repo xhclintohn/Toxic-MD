93c6d147a15c2bdc79c06feebd786d9807129f41
𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 <152104512+xhclintohn@users.noreply.github.com>
2025-04-07 06:01:01 +0300

    Update toxic.js

--- commandes/toxic.js
+++ commandes/toxic.js
@@ -1,16 +1,25 @@
-zokou({
-    nomCom: "bot",  /
-    categorie: "General",
-    reaction: "🤖" 
-}, async (dest, zk, commandeOptions) => {
-    const { repondre } = commandeOptions;
-    
-   
-    const fancyResponse = `
-𝐇𝐞𝐥𝐥𝐨! 𝐇𝐨𝐰 𝐜𝐚𝐧 𝐈 𝐡𝐞𝐥𝐩🙂🤚🏻
-    
+const { zokou } = require("../framework/zokou");
+
+module.exports = {
+    name: "bot", // Command name
+    description: "Greets the user", // Command description
+    usage: ".bot", // Command usage
+    enable: true, // Enable the command
+
+    zokou({ 
+        nomCom: "bot", // cmd
+        categorie: "General",
+        reaction: "🤖" 
+    }, async (dest, zk, commandeOptions) => {
+        const { repondre } = commandeOptions;
+        
+        
+        const fancyResponse = `
+◄⏤͟͞𝐇𝐞𝐥𝐥𝐨! 𝐇𝐨𝐰 𝐜𝐚𝐧 𝐈 𝐡𝐞𝐥𝐩🙂🤚🏻
+
 > ✗𝗗
-    `;
-    
-    await repondre(fancyResponse);
-});
+        `;
+        
+        await repondre(fancyResponse);
+    })
+};
