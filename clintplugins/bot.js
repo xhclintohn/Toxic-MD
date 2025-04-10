2b1cc9e5fc5c87b858cce98c1f59fac98b4ab13e
𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 <152104512+xhclintohn@users.noreply.github.com>
2025-04-10 05:48:33 +0300

    Add files via upload

--- clintplugins/bot.js
+++ clintplugins/bot.js
@@ -64,7 +64,7 @@ STATUS
         }
 
         // Send audio file from root directory
-        const audioPath = path.join(__dirname, '/../Royalty.m4a'); // Changed path
+        const audioPath = path.join(__dirname, './𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧/bot.m4a'); // Changed path
         if (fs.existsSync(audioPath)) {
             await zk.sendMessage(dest, {
                 audio: { url: audioPath },
