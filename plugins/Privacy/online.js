733a511f71ae080f10df084bb345843323d9dbe4
xh_clinton <152104512+xhclintohn@users.noreply.github.com>
2026-06-23 01:18:33 +0300

    fix: remove duplicate ButtonV2 setFooter watermark

--- plugins/Privacy/online.js
+++ plugins/Privacy/online.js
@@ -34,7 +34,7 @@ export default async (context) => {
         try {
             const btnV2 = new ButtonV2(client);
             btnV2.setBody(bodyText)
-                .setFooter('> ©𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧')
+
                 .addButton('All ✅', `${prefix}online all`)
                 .addButton('Match Last Seen 🕒', `${prefix}online match_last_seen`);
             await btnV2.send(m.chat, { userJid: client.user?.id || '' });
